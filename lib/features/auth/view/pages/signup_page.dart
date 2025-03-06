7e3cc93581076ffc804da05032ce2df81edab37b
jeevan2304 <jeevanhg2003@gmail.com>
2025-03-06 20:54:45 +0530

    sign up

--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/theme/app_pallette.dart';
+import 'package:spotify/features/auth/view/widgets/auth_gradient_button.dart';
+import 'package:spotify/features/auth/view/widgets/custom_field.dart';
 
 class  SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,6 +13,46 @@ class  SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign Up.',
+              style: TextStyle(
+                  fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30,),
+            CustomField(
+              hintText: 'Name',
+            ),
+            const SizedBox(height: 15,),
+            CustomField(hintText: 'Email'),
+            const SizedBox(height: 15,),
+            CustomField(hintText: 'Password'),
+            const SizedBox(height: 15,),
+            AuthGradientButton(),
+            const SizedBox(height: 15,),
+            RichText(text: TextSpan(text: 'Already have an account? ',
+              style: Theme.of(context).textTheme.titleMedium,
+              children:[
+                TextSpan(text: 'Sign In',
+                    style: TextStyle(color: Pallete.gradient2,
+                    fontWeight: FontWeight.bold,),
+                ),
+              ],
+
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+
   }
 }
